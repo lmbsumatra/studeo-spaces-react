c66556fce6ba92f01d85eaafe41bdac645ed343d
Kurisu220 <manzanojc02@gmail.com>
2025-01-08 22:38:23 +0800

    Revert "Cancel button in booking details and removing the ShouldQueue"
    This reverts commit e610cb1dc40259a7e6845b876e46efdf1a1886dd.

--- backend/app/Http/Controllers/BookingController.php
+++ backend/app/Http/Controllers/BookingController.php
@@ -59,8 +59,7 @@ class BookingController extends Controller
             $validatedData['service_name'] = $service->name;
     
             // Determine pass type and payment method
-            $customer = $this->findOrCreateCustomer($validatedData);
-            $passDetails = $this->determinePassType($customer);
+            $passDetails = $this->determinePassType($customer = $this->findOrCreateCustomer($validatedData));
             $validatedData = array_merge($validatedData, $passDetails);
     
             // Begin database transaction
@@ -509,9 +508,9 @@ class BookingController extends Controller
             }
 
             // Update booking status to 'Canceled'
-            $booking->status = 'Cancelled';
+            $booking->status = 'Canceled';
             $booking->save();
-            Log::info("Booking status updated to 'Cancelled' for refNumber: $refNumber");
+            Log::info("Booking status updated to 'Canceled' for refNumber: $refNumber");
 
             // Send cancellation confirmation email
             Mail::to($booking->email)->send(new CancellationConfirmationMail($booking));
